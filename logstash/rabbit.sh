e363be4cdc1954a4d0ec7fa38bbe69d854ac352d
irom77 <irek.romaniuk@gmail.com>
2017-03-04 11:01:08 -0500

    client and server are working with syslog-generator

--- logstash/rabbit.sh
+++ logstash/rabbit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker stop rabbit
 docker rm rabbit 
-docker run -d --network rabbit --hostname rabbit --name rabbit -p 5672:5672 -p 15672:15672 -e RABBITMQ_DEFAULT_USER=admin -e RABBITMQ_DEFAULT_PASS=n3w@yn rabbitmq:3-management
+docker run -d --network rabbit --hostname rabbit --name rabbit -p 5672:5672 -p 15672:15672 -e RABBITMQ_DEFAULT_USER=admin -e RABBITMQ_DEFAULT_PASS=rabbit rabbitmq:3-management
